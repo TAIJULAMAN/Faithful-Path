9f09f98deda20718b6429b62cccd8253008f5018
Md Shah Aman Patwary <taijulaman8@gmail.com>
2024-08-13 07:43:56 +0600

    first 40 surah is completed

--- src/pages/Quran/Quran.jsx
+++ src/pages/Quran/Quran.jsx
@@ -1,6 +1,18 @@
 import surahInfo from "../../../public/Quran/surahInfo.json";
 import { Link } from "react-router-dom";
 
+// {
+//   "id": 1,
+//   "number": "১",
+//   "name": "আল-ফাতিহা",
+//   "means": "সূচনা",
+//   "arabic": "سُورَةُ ٱلْفَاتِحَةِ",
+//   "url": "Al-Fatihah",
+//   "english": "Al-Fatihah",
+//   "verse_num_en": 7,
+//   "verse_num_bn": "৭"
+// },
+
 function Quran() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3  font-bold mx-auto gap-5 p-5 bg-[#F7F3DE] text-[#2e424d] px-24">
--- src/pages/Quran/QuranDetails.jsx
+++ src/pages/Quran/QuranDetails.jsx
@@ -1,6 +1,21 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
+
+// {
+//   "id": 1,
+//   "number": "১",
+//   "name": "আল-ফাতিহা",
+//   "means": "সূচনা",
+//   "arabic": "سُورَةُ ٱلْفَاتِحَةِ",
+//   "url": "Al-Fatihah",
+//   "english": "Al-Fatihah",
+//   "verse_num_en": 7,
+//   "verse_num_bn": "৭"
+// },  
+
+
+
 function QuranDetails() {
   const [surah, setSurah] = useState([]);
   const router = useLocation();
